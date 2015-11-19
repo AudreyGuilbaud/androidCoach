dd78a0a7c34354338c5d9aa6480f1eca0c946be1
AudreyGuilbaud <guilbaud.audrey@live.fr>
2015-11-19 19:06:42 +0100

    Suppression de l'icone mail

--- src/main/java/com/example/sessionprincipale/coach/CoachActivity.java
+++ src/main/java/com/example/sessionprincipale/coach/CoachActivity.java
@@ -132,15 +132,6 @@ public class CoachActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coach);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         this.ecouteRadio();
         this.ecouteCalcul();
     }
